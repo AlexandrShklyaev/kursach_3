c51fd0b852c62acd863684c835ceb24b6413a0a5
AlexandrShklyaev <upravbal@gmail.com>
2022-11-07 00:33:15 +0400

    init

--- app/config.py
+++ app/config.py
@@ -1,3 +1,2 @@
 class Config:
-    DUBUG = True
     SQLALCHEMY_DATABASE_URI = "sqlite:///db_users.db"
--- app/models.py
+++ app/models.py
@@ -2,7 +2,8 @@ from app.database import db
 
 
 class Employeer(db.Model):
-    __tablename__= "users"
+    """ описание модели таблицы 'users' """
+    __tablename__ = "users"
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(200))
     email = db.Column(db.String(100))
